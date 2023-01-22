31ae470346e957de162f74c915f56e043173640c
Siu Kei CHEUNG <siukei.cheung.hk@gmail.com>
2023-01-22 11:43:20 +0100

    Replace Redis with DragonflyDB for test and revert change in view config file

--- config/view.php
+++ config/view.php
@@ -29,7 +29,7 @@ return [
 
     'compiled' => env(
         'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
+        realpath(storage_path('framework/views/blade'))
     ),
 
     /*
